a4f7acf9995ec11a5daacb58b2fa3080622006c8
Wouter Vandenneucker <wouter@vandenneucker.be>
2014-01-06 19:59:10 +0100

    rerefix

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    require(get_template_directory_uri() . "/logincheck.php");
+    require("logincheck.php");
 ?>
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
