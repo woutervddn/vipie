821f491ecd594696c70d4b7ab8d967f673c5a214
Wouter Vandenneucker <wouter@vandenneucker.be>
2014-01-06 21:56:38 +0100

    blash

--- vipie-template.php
+++ vipie-template.php
@@ -275,7 +275,7 @@ Template Name: Vipie Template
                 theArticleId = object.attr("href");
                 theArticlePosition = $(".article-container" + theArticleId).position().left;
                 theScroll = theArticlePosition - $(".paper-container").scrollLeft();
-                console.log($(".paper-container").scrollLeft());
+                console.log($(".paper-container").scrollLeft() +" to "+theArticlePosition+" = "+theScroll );
                 $(".paper-container").animate({scrollLeft: theScroll}, 800);
                 
             });
