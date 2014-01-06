6e33107c993e51951fe7cbcc35f4b9589b38e267
Wouter Vandenneucker <wouter@vandenneucker.be>
2014-01-06 20:06:42 +0100

    fix logincheck

--- logincheck.php
+++ logincheck.php
@@ -1,6 +1,6 @@
 <?php if (!(current_user_can('level_0'))){
     
-    $location=bloginfo('url') . "/login";
+    $location=bloginfo('url') . "/login.php";
     
     wp_redirect( $location );
     exit;
